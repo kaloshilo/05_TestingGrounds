9a57ed12ef3bcc8991bc7914a57fc0e786a30b2c
Ronald Bradshaw <kaloshilo@hotmail.com>
2017-06-04 13:56:18 -0600

    Lesson 214

--- Source/S05_TestingGrounds/ChooseNextWaypoint.h
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.h
@@ -6,14 +6,17 @@
 #include "ChooseNextWaypoint.generated.h"
 
 /**
- * 
- */
+*
+*/
 UCLASS()
 class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
+
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
-	
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
+
 };
