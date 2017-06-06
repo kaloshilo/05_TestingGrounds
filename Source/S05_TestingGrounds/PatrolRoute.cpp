5142a1b544d7ae9e75e838f9efaf9861cb92edbd
Ronald Bradshaw <kaloshilo@hotmail.com>
2017-06-06 12:27:34 -0600

    Lesson 219

--- Source/S05_TestingGrounds/PatrolRoute.cpp
+++ Source/S05_TestingGrounds/PatrolRoute.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "S05_TestingGrounds.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
-
-
-
+TArray<AActor*> UPatrolRoute::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
